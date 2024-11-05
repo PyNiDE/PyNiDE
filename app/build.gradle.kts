9858481d75e9a5a0a21f808943493bcda125233e
RohitVerma882 <rohitkumar882333@gmail.com>
2024-11-05 16:40:34 +0530

    add new launcher icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.ir.backend.js.compile
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.application)
+    alias(libs.plugins.kotlin)
 }
 
 android {
@@ -12,12 +14,17 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -28,15 +35,32 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = true
+
+        disable += "MissingTranslation"
+        disable += "ExtraTranslation"
+        disable += "BlockedPrivateApi"
+    }
+}
+
+configurations.all {
+    exclude(group = "androidx.recyclerview", module = "recyclerview")
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.application) apply false
+    alias(libs.plugins.kotlin) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
